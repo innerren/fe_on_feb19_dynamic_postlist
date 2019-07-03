c641ef1be12175b68342e9ee0990721bf7ffd4ac
innerren <innerren@gmail.com>
2019-07-03 17:56:21 +0300

    Updates

---
+++ precache-manifest.0f83eabecb4fd9ec3da0a08652dcdb4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf94cfed68712c0e6cd850e7947f264",
+    "url": "/index.html"
+  },
+  {
+    "revision": "391db096f488bf0bb1d3",
+    "url": "/static/css/main.fd30be2b.chunk.css"
+  },
+  {
+    "revision": "2acaddf16f44653bfebf",
+    "url": "/static/js/2.ae634932.chunk.js"
+  },
+  {
+    "revision": "391db096f488bf0bb1d3",
+    "url": "/static/js/main.52d68663.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
